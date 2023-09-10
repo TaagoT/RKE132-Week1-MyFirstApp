3c5dba8a3bcb053a1d3c7aa3acaadc3b765f5767
TaagoT <taago.tagam@tktk.ee>
2023-09-10 11:34:13 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿//This is a comment to my code
+
+//rakendus küsib kasutajal sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:"); //Väljund (output)
+//string - sõne
+string userName = Console.ReadLine(); //Sisend (input)
+
+Console.WriteLine("Hello" + ", " + userName + "!"); //Output
+
+//string interpolation
+Console.WriteLine($"Hello, {userName} !"); //Output
